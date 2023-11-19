1cdd763f2f24c67e48400965e7338ca1cd950af9
WebDevilishSmile <webdevilishsmile@gmail.com>
2023-11-18 23:05:58 -0500

    11/18/2023 23:00

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -25,7 +25,6 @@ export default function Login() {
         backgroundColor: theme.palette.background.light,
       }}
     >
-      {`${location}/api/auth/callback`}
       <Auth
         supabaseClient={supabase}
         appearance={{ theme: ThemeSupa }}

eea933760a16d905eacdc2e115c890176886c2e5
Tiago <webdevilishsmile@gmail.com>
2025-05-25 23:13:21 -0400

    5/25/2025 23:13

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,23 +1,35 @@
 import { AppBar, Button, ListItem, Toolbar, Typography } from '@mui/material';
 import UserMenu from './UserMenu';
-import { auth } from '@/auth';
+import { auth, signIn, signOut } from '@/auth';
 
 export default async function Header() {
-  const session = auth();
+  const session = await auth();
 
   return (
     <AppBar>
       <Toolbar>
-        <UserMenu>
-          <ListItem
-            component='form'
-            action={async () => {
-              'use server';
-              console.log('SIGN IN');
-            }}
-          >
-            <Button type='submit'>Sign In</Button>
-          </ListItem>
+        <UserMenu user={session?.user}>
+          {session?.user ? (
+            <ListItem
+              component='form'
+              action={async () => {
+                'use server';
+                await signOut({ redirectTo: '/signin' });
+              }}
+            >
+              <Button type='submit'>Sign Out</Button>
+            </ListItem>
+          ) : (
+            <ListItem
+              component='form'
+              action={async () => {
+                'use server';
+                await signIn('google');
+              }}
+            >
+              <Button type='submit'>Sign In</Button>
+            </ListItem>
+          )}
         </UserMenu>
 
         <Typography>Header</Typography>
--- auth.js
+++ auth.js
@@ -1,5 +1,9 @@
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import Google from 'next-auth/providers/google';
 import NextAuth from 'next-auth';
+import client from './lib/db';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: MongoDBAdapter(client),
+  providers: [Google],
 });

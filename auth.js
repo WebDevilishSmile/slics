40744b2da60938b02ca4e4c054c05cc26ffc35b5
Tiago <webdevilishsmile@gmail.com>
2025-05-29 20:23:46 -0400

    5/29/2025 20:23

--- auth.js
+++ auth.js
@@ -6,7 +6,17 @@ import { ObjectId } from 'mongodb';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(client),
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: 'select_account',
+        },
+      },
+    }),
+  ],
   events: {
     async createUser(message) {
       const db = client.db();

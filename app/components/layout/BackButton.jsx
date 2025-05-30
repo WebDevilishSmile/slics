05455bb629460aea638dd10cc5666fb65e7c0918
Tiago <webdevilishsmile@gmail.com>
2025-05-29 23:39:22 -0400

    5/29/2025 23:39

--- app/components/layout/BackButton.jsx
+++ app/components/layout/BackButton.jsx
@@ -13,7 +13,7 @@ function BackButton() {
 
   return (
     <Button
-      sx={{ position: 'absolute', top: '4.2rem', right: '1rem' }}
+      sx={{ position: 'absolute', top: '4.8rem', right: '1rem' }}
       onClick={handleBack}
     >
       <ChevronLeftOutlined /> Back

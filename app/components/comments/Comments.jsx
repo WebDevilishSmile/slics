683a62fa0391bf9299c8b98c01e0a26d678fded9
Tiago <webdevilishsmile@gmail.com>
2025-06-21 22:38:11 -0400

    6/21/2025 22:38

--- app/components/comments/Comments.jsx
+++ app/components/comments/Comments.jsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { CircularProgress, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { useSearchParams } from 'next/navigation';
-import { Suspense, useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import Comment from './Comment';
 import CommentsContainer from './CommentsContainer';
 import NoSlicComments from './NoSlicComments';
--- app/components/comments/CommentsContainer.jsx
+++ app/components/comments/CommentsContainer.jsx
@@ -1,16 +1,39 @@
 'use client';
 
-import { Button, Collapse, IconButton, Paper, Typography } from '@mui/material';
-import CommentEditor from './CommentEditor';
-import { AddComment, HideSource } from '@mui/icons-material';
-import { useState } from 'react';
 import { ELEVATION, MAX_WIDTH, MIN_HEIGHT } from '@/utils/variables';
+import { AddComment, HideSource, ImageOutlined } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  Collapse,
+  IconButton,
+  Paper,
+  TextField,
+  Typography,
+} from '@mui/material';
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+import CommentEditor from './CommentEditor';
 
 function CommentsContainer({ children, user, numSlic, refetchComments }) {
   const [showEditor, setShowEditor] = useState(false);
+  const [showImage, setShowImage] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const fileInputRef = useRef(null);
 
   const toggleEditor = () => {
     setShowEditor((prev) => !prev);
+  };
+
+  const toggleImage = () => {
+    setShowImage((prev) => !prev);
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedImage(URL.createObjectURL(file));
+    }
   };
 
   return (
@@ -29,6 +52,14 @@ function CommentsContainer({ children, user, numSlic, refetchComments }) {
         px: '1rem',
       }}
     >
+      <IconButton
+        sx={{ position: 'absolute', top: '2rem', left: '2rem' }}
+        onClick={toggleImage}
+        disabled={!numSlic}
+      >
+        <ImageOutlined />
+      </IconButton>
+
       <Typography variant='h4' sx={{ textAlign: 'center', mb: '1rem' }}>
         Comments
       </Typography>
@@ -49,6 +80,40 @@ function CommentsContainer({ children, user, numSlic, refetchComments }) {
           setShowEditor={setShowEditor}
         />
       </Collapse>
+
+      <Collapse in={showImage} sx={{ width: '100%' }}>
+        <TextField
+          sx={{ my: '1rem' }}
+          type='file'
+          accept='image/*'
+          onChange={handleImageChange}
+        />
+
+        {selectedImage && (
+          <Box
+            sx={{
+              mb: '1rem',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Box sx={{ position: 'relative', width: '100%', height: '18rem' }}>
+              <Image
+                src={selectedImage}
+                alt='Selected'
+                fill
+                style={{ maxWidth: '100%', objectFit: 'contain' }}
+              />
+            </Box>
+
+            <Button variant='contained' color='primary' sx={{ mt: '1rem' }}>
+              Upload
+            </Button>
+          </Box>
+        )}
+      </Collapse>
+
       {children}
     </Paper>
   );
